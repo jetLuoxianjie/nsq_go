3017c02c3d7f2a6551ef4dcbaf331deeba66bb7b
Jet-luoxianjie <31613086+Jet-luoxianjie@users.noreply.github.com>
2022-10-10 16:33:39 +0800

    fix

--- nsq_producer.go
+++ nsq_producer.go
@@ -6,139 +6,145 @@ import (
 	"github.com/nsqio/go-nsq"
 	"go.uber.org/zap"
 	"strings"
+	"sync/atomic"
 	"time"
 )
 
-var myNsqdMgr *nsqdMgr
+var nsqdManager *nsqdMgr
 
-//nsqd 管理
+// nsqdMgr nsqd管理
 type nsqdMgr struct {
-	//生产者nsq列表
+	// 生产者nsq列表
 	producerList []*nsqdProducer
-	currIndex    int //当前发消息的生产者下标
 
-	//记录当前所有nsq的 信息 (主要用于判断更新的nsq是否新增)
-	nodesTcpMap  map[string]struct{} //k:tcp地址
-	nodesHttpMap map[string]struct{} //k:http地址
+	// 当前发消息的生产者下标
+	currIndex int32
 
+	// 记录当前所有nsq的 信息 (主要用于判断更新的nsq是否新增)
+	//k:tcp地址
+	nodesTcpMap map[string]struct{}
+	//k:http地址
+	nodesHttpMap map[string]struct{}
 }
 
-//nsqd生成者
+// nsqdProducer nsqd生成者
 type nsqdProducer struct {
-	startTime int64 //启动时间
+	// 启动时间
+	startTime int64
 	producer  *nsq.Producer
 }
 
-//nsq节点列表 (用于从 nsqlookup 获取消息 解析使用)
+// nsqNodeList nsq节点列表，用于从 nsqlookup 获取消息 解析使用
 type nsqNodeList struct {
 	Producers []*nsqdNode `json:"producers"`
 }
 
-//nsqd节点信息
+// nsqdNode nsqd节点信息
 type nsqdNode struct {
-	RemoteAddress    string   `json:"remote_address"` //nsqd的远端地址(ip+端口)
+	RemoteAddress    string   `json:"remote_address"` // nsqd的远端地址(ip+端口)
 	HostName         string   `json:"host_name"`
 	BroadcastAddress string   `json:"broadcast_address"`
-	TcpPort          int      `json:"tcp_port"`  //tcp端口
-	HttpPort         int      `json:"http_port"` //http端口
+	TcpPort          int      `json:"tcp_port"`
+	HttpPort         int      `json:"http_port"`
 	Version          string   `json:"version"`
 	Topics           []string `json:"topics"`
 }
 
-//初始化nsq生产者
+// InitNsqProducer 初始化nsq生产者
 func InitNsqProducer(nsqConfig *nsq.Config, updateNsqInterval int) {
 	if myNsqGo == nil {
-		nsqGoLogError("nsqGo not start")
+		nsqGoLogError("[nsq] not start")
 		return
 	}
 	if nsqConfig == nil {
-		nsqGoLogError("nsqConfig nil")
+		nsqGoLogError("[nsq] config nil")
 		return
 	}
-	if myNsqdMgr == nil {
-		myNsqdMgr = &nsqdMgr{
+	if nsqdManager == nil {
+		nsqdManager = &nsqdMgr{
 			currIndex:    0,
 			nodesTcpMap:  make(map[string]struct{}),
 			nodesHttpMap: make(map[string]struct{}),
 		}
 	}
-	//更新节点信息
+	// 更新节点信息
 	updateNsqdNodes(nsqConfig)
-	//定时更新
+	// 定时更新
 	nsqGoSetTimeout(updateNsqInterval, func(args ...interface{}) int {
 		updateNsqdNodes(nsqConfig)
 		return updateNsqInterval
 	})
-	nsqGoLogInfo("init nsq producer successful !!!!")
+	nsqGoLogInfo("[nsq] init producer successful")
 }
 
-//发送消息 //采用轮训发送    //isSync是否同步参数 isSync填写该值就是同步，不填默认异步
+// NsqPush 发送消息，采用轮训发送
+// isSync是否同步参数，isSync填写该值就是同步，不填默认异步
 func NsqPush(topic string, data []byte, isSync ...interface{}) bool {
-	if myNsqdMgr == nil {
-		nsqGoLogError("nsqdMgr nil")
+	if nsqdManager == nil {
+		nsqGoLogError("[nsq] manager nil")
 		return false
 	}
-	lenList := len(myNsqdMgr.producerList)
-	//给一个nsqd发送即可,若发送失败就发送下一个
+	lenList := len(nsqdManager.producerList)
+	// 给一个nsqd发送即可,若发送失败就发送下一个
 	for i := 0; i < lenList; i++ {
-		nsqGoLogInfo("【nsq】发送消息", zap.String("Topic", topic), zap.Int64("time", time.Now().Unix()))
-		//这次感觉使不使用原子操作都影响不大
-		myNsqdMgr.currIndex++
-		myNsqdMgr.currIndex = myNsqdMgr.currIndex % lenList
-		if myNsqdMgr.currIndex > lenList-1 {
-			nsqGoLogError("currIndex out")
-			return false
+		nsqGoLogDebug("[nsq] send message", zap.String("topic", topic))
+		// TODO: 这次感觉使不使用原子操作都影响不大?
+		nsqdManager.currIndex++
+		index := atomic.AddInt32(&nsqdManager.currIndex, 1) % int32(lenList)
+		if index == 0 {
+			atomic.StoreInt32(&nsqdManager.currIndex, 0)
 		}
-		nsqGoProducer := myNsqdMgr.producerList[myNsqdMgr.currIndex]
+		nsqGoProducer := nsqdManager.producerList[index]
 		if nsqGoProducer == nil {
-			nsqGoLogError("nsqGoProducer nil")
+			nsqGoLogError("[nsq] producer nil")
 			continue
 		}
 		if nsqGoProducer.producer == nil {
-			nsqGoLogError("nsqGoProducer producer nil")
+			nsqGoLogError("[nsq] producer.producer nil")
 			continue
 		}
-		//防止消费者还未发现新添加的nsqd
-		if nsqGoProducer.startTime+myNsqGo.lookupdPollInterval >= time.Now().Unix() {
+		// 防止消费者还未发现新添加的nsqd
+		if nsqGoProducer.startTime+myNsqGo.nsqlookupPollInterval >= time.Now().Unix() {
 			continue
 		}
 		if len(isSync) > 0 {
 			err := nsqGoProducer.producer.Publish(topic, data)
 			if err != nil {
-				nsqGoLogError("nsq  PublishAsync err", zap.Error(err))
+				nsqGoLogError("[nsq] publishAsync err", zap.Error(err))
 				continue
 			}
 		} else {
 			err := nsqGoProducer.producer.PublishAsync(topic, data, nil)
 			if err != nil {
-				nsqGoLogError("nsq  PublishAsync err", zap.Error(err))
+				nsqGoLogError("[nsq] publishAsync err", zap.Error(err))
 				continue
 			}
 		}
 		return true
 	}
-	nsqGoLogError("all producer Publish err", zap.Int("len", lenList))
+	nsqGoLogError("[nsq] all producer publish err", zap.Int("len", lenList))
 	return false
 }
 
-//更新nsqd的节点信息 （会填充到 Config.Server.NsqConfig.nsqlookupHttpAddress Config.Server.NsqConfig.NsqdTcpAddress 中）
+// updateNsqdNodes 更新nsqd的节点信息
+// 会填充到 Config.Server.NsqConfig.nsqlookupHttpAddress,Config.Server.NsqConfig.NsqdTcpAddress 中
 func updateNsqdNodes(nsqConfig *nsq.Config) {
 	if nsqConfig == nil {
-		nsqGoLogError("nsqConfig nil")
+		nsqGoLogError("[nsq] config nil")
 		return
 	}
 	if myNsqGo == nil {
-		nsqGoLogError("nsqGo not start")
+		nsqGoLogError("[nsq] not start")
 		return
 	}
-	if myNsqdMgr == nil {
-		nsqGoLogError("nsqdMgr nil")
+	if nsqdManager == nil {
+		nsqGoLogError("[nsq] manager is nil")
 		return
 	}
-	//增加的nsqd节点
+	// 增加的nsqd节点
 	addNodesTcpMap := make(map[string]struct{})
 	addNodesHttpMap := make(map[string]struct{})
-	//当前的nsqd连接 （会删除一些没有使用的）
+	// 当前的nsqd连接 （会删除一些没有使用的）
 	currNodesTcpMap := make(map[string]struct{})
 	for _, address := range myNsqGo.nsqlookupHttpAddress {
 		url := fmt.Sprintf("http://%v/nodes", address)
@@ -149,7 +155,7 @@ func updateNsqdNodes(nsqConfig *nsq.Config) {
 		nl := &nsqNodeList{}
 		err := json.Unmarshal(body, nl)
 		if err != nil {
-			nsqGoLogError("Unmarshal err", zap.Error(err))
+			nsqGoLogError("[nsq] json unmarshal err", zap.Error(err))
 			return
 		}
 		for _, v := range nl.Producers {
@@ -158,7 +164,7 @@ func updateNsqdNodes(nsqConfig *nsq.Config) {
 			}
 			strList := strings.Split(v.RemoteAddress, ":")
 			if len(strList) != 2 {
-				nsqGoLogError("RemoteAddress err", zap.String("RemoteAddress", v.RemoteAddress))
+				nsqGoLogError("[nsq] remoteAddress err", zap.String("remoteAddress", v.RemoteAddress))
 				continue
 			}
 			ip := strList[0]
@@ -166,55 +172,56 @@ func updateNsqdNodes(nsqConfig *nsq.Config) {
 				ip = strings.Split(address, ":")[0]
 			}
 			tcpAddress := fmt.Sprintf("%v:%v", ip, v.TcpPort)
-			//填充tcpMap
-			if _, ok := myNsqdMgr.nodesTcpMap[tcpAddress]; !ok {
-				myNsqdMgr.nodesTcpMap[tcpAddress] = struct{}{}
-				//增加Tcp
+			// 填充tcpMap
+			if _, ok := nsqdManager.nodesTcpMap[tcpAddress]; !ok {
+				nsqdManager.nodesTcpMap[tcpAddress] = struct{}{}
+				// 增加Tcp
 				addNodesTcpMap[tcpAddress] = struct{}{}
 			}
-			//记录当前的
+			// 记录当前的
 			currNodesTcpMap[tcpAddress] = struct{}{}
 
 			httpAddress := fmt.Sprintf("%v:%v", ip, v.HttpPort)
-			//填充HttpMap
-			if _, ok := myNsqdMgr.nodesHttpMap[httpAddress]; !ok {
-				myNsqdMgr.nodesHttpMap[httpAddress] = struct{}{}
-				//增加http
+			// 填充HttpMap
+			if _, ok := nsqdManager.nodesHttpMap[httpAddress]; !ok {
+				nsqdManager.nodesHttpMap[httpAddress] = struct{}{}
+				// 增加http
 				addNodesHttpMap[httpAddress] = struct{}{}
 			}
 		}
 	}
 	isFirst := len(myNsqGo.nsqdTcpAddress) == 0
-	//填充 增加的
-	for address, _ := range addNodesTcpMap {
+	// 填充 增加的
+	for address := range addNodesTcpMap {
 		myNsqGo.nsqdTcpAddress = append(myNsqGo.nsqdTcpAddress, address)
-		//增加nsqd的生产者
+		// 增加nsqd的生产者
 		producer, err := nsq.NewProducer(address, nsqConfig)
 		if err != nil {
-			nsqGoLogError("create producer failed, err", zap.Error(err))
+			nsqGoLogError("[nsq] create producer failed", zap.Error(err))
 		}
 		startTime := time.Now().Unix()
 		if isFirst {
-			//第一次可以立即使用
+			// 第一次可以立即使用
 			startTime = 0
 		}
-		myNsqdMgr.producerList = append(myNsqdMgr.producerList, &nsqdProducer{
+		nsqdManager.producerList = append(nsqdManager.producerList, &nsqdProducer{
 			startTime: startTime,
 			producer:  producer,
 		})
 	}
-	for address, _ := range addNodesHttpMap {
+	for address := range addNodesHttpMap {
 		myNsqGo.nsqdHttpAddress = append(myNsqGo.nsqdHttpAddress, address)
 	}
 
-	//删除没有注册的
-	for i, p := range myNsqdMgr.producerList {
+	// 删除没有注册的
+	for i := 0; i < len(nsqdManager.producerList); i++ {
+		p := nsqdManager.producerList[i]
 		if p == nil || p.producer == nil {
-			nsqGoLogError("nil", zap.Int("index", i))
+			nsqGoLogError("[nsq] producer nil", zap.Int("index", i))
 			continue
 		}
 		isDel := true
-		for add, _ := range currNodesTcpMap {
+		for add := range currNodesTcpMap {
 			if p.producer.String() == add {
 				isDel = false
 				break
@@ -223,28 +230,31 @@ func updateNsqdNodes(nsqConfig *nsq.Config) {
 		if !isDel {
 			continue
 		}
-		//判断下 但是基本不可能  //slice中 如: arr:=[]int{1}  arr[len(arr):]这样是合法的√   arr[len(arr)]这样才是下标越界×
-		if i+1 > len(myNsqdMgr.producerList) {
-			nsqGoLogError("arr index out", zap.Int("i+1", i+1), zap.Int("len", len(myNsqdMgr.producerList)))
+		// 判断下 但是基本不可能
+		// slice中 如: arr:=[]int{1}  arr[len(arr):]这样是合法的√
+		// arr[len(arr)]这样才是下标越界×
+		if i+1 > len(nsqdManager.producerList) {
+			nsqGoLogError("arr index out", zap.Int("i+1", i+1), zap.Int("len", len(nsqdManager.producerList)))
 			continue
 		}
-		//停止
+		// 停止
 		p.producer.Stop()
-		//producerList 删除
-		myNsqdMgr.producerList = append(myNsqdMgr.producerList[:i], myNsqdMgr.producerList[i+1:]...)
-		//NodesTcpMap 删除
-		delete(myNsqdMgr.nodesTcpMap, p.producer.String())
-		//重置currIndex
-		myNsqdMgr.currIndex = 0
+		// producerList 删除
+		nsqdManager.producerList = append(nsqdManager.producerList[:i], nsqdManager.producerList[i+1:]...)
+		i--
+		// NodesTcpMap 删除
+		delete(nsqdManager.nodesTcpMap, p.producer.String())
+		// 重置currIndex
+		nsqdManager.currIndex = 0
 	}
 }
 
-//获得nsqd数量
+// GetNsqdNum 获得nsqd数量
 func GetNsqdNum() int {
-	if myNsqdMgr == nil {
-		nsqGoLogError("nsqdMgr nil")
+	if nsqdManager == nil {
+		nsqGoLogError("[nsq] manager nil")
 		return 0
 	}
-	return len(myNsqdMgr.producerList)
+	return len(nsqdManager.producerList)
 
 }
